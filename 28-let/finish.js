31f56c8a2360786ebabea267f78618ed8df0d3cb
Rita Sol <ritinarita559@gmail.com>
2024-06-04 16:18:31 +0300

    решение

--- 28-let/finish.js
+++ 28-let/finish.js
@@ -3,12 +3,23 @@
  * Измените код так, чтобы ошибка исчезла
  */
 
-const myFavoriteAnimal = 'Monkey'
+// const myFavoriteAnimal = 'Monkey'
 
-console.log(myFavoriteAnimal)
-// 'Monkey'
+// console.log(myFavoriteAnimal)
+// // 'Monkey'
 
-const myFavoriteAnimal = 'Cat'
+// const myFavoriteAnimal2 = 'Cat'
 
-console.log(myFavoriteAnimal)
-// 'Cat'
+// console.log(myFavoriteAnimal2)
+// // 'Cat'
+
+
+// let myFavoriteAnimal = 'Monkey'
+
+// console.log(myFavoriteAnimal)
+// // 'Monkey'
+
+// myFavoriteAnimal = 'Cat'
+
+// console.log(myFavoriteAnimal)
+// // 'Cat'
